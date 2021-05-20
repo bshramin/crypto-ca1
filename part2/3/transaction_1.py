97d67d929d2a85eccb11a93686a9815b6891bb04
Amin Bashiri <amin.bashiri28@gmail.com>
2021-05-20 17:51:52 +0430

    part 2.3

--- part2/3/transaction_1.py
+++ part2/3/transaction_1.py
@@ -1,34 +1,46 @@
+from bitcoin.core.script import OP_EQUAL, OP_HASH160
 import bitcoin.wallet
 from bitcoin.core import COIN, b2lx, serialize, x, lx, b2x
 from utils import *
 
-bitcoin.SelectParams("testnet") // Select the network (testnet or mainnet)
-my_private_key = bitcoin.wallet.CBitcoinSecret("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") # Private key in WIF format XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+bitcoin.SelectParams("testnet") # Select the network (testnet or mainnet)
+my_private_key = bitcoin.wallet.CBitcoinSecret("92gpofwfthgN6risZNHK87Z7ynaTGXPgcVmgFNP22xEsFHJvHn9") # Private key in WIF format XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 my_public_key = my_private_key.pub
 my_address = bitcoin.wallet.P2PKHBitcoinAddress.from_pubkey(my_public_key)
-destination_address = bitcoin.wallet.CBitcoinAddress('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') # Destination address (recipient of the money)
+destination_address = bitcoin.wallet.CBitcoinAddress('n2yhmR6LTUwtpYSm5ut39zMAmPiYBfShxN') # Destination address (recipient of the money)
 
-def P2PKH_scriptPubKey(address):
+
+# to condition spending the outputs
+def P2PKH_scriptPubKey_out(address):
+    ######################################################################
+    ## Fill out the operations for P2PKH scriptPubKey                   ##
+    script_hash = b'748284390f9e263a4b766a75d0633c50426eb875'
+    return [OP_HASH160, script_hash, OP_EQUAL]
+
+
+# to condition spending the outputs
+def P2PKH_scriptPubKey_in(address):
     ######################################################################
     ## Fill out the operations for P2PKH scriptPubKey                   ##
 
-    return [ ... ,OP_CHECKSIG] #Fill this section
-    ######################################################################
+    return [OP_DUP, OP_HASH160, my_address, OP_EQUALVERIFY, OP_CHECKSIG]
 
+
+# to show that you own the inputs
 def P2PKH_scriptSig(txin, txout, txin_scriptPubKey):
     ######################################################################
     ## Fill out the operations for P2PKH scriptSig                      ##
 
     signature = create_OP_CHECKSIG_signature(txin, txout, txin_scriptPubKey, my_private_key)
 
-    return [signature, ... ] #Fill this section
-    ######################################################################
+    return [signature, my_public_key]
+
 
 def send_from_P2PKH_transaction(amount_to_send, txid_to_spend, utxo_index,
                                 txout_scriptPubKey):
     txout = create_txout(amount_to_send, txout_scriptPubKey)
 
-    txin_scriptPubKey = P2PKH_scriptPubKey(my_address)
+    txin_scriptPubKey = P2PKH_scriptPubKey_in(my_address)
     txin = create_txin(txid_to_spend, utxo_index)
     txin_scriptSig = P2PKH_scriptSig(txin, txout, txin_scriptPubKey)
 
@@ -39,16 +51,16 @@ def send_from_P2PKH_transaction(amount_to_send, txid_to_spend, utxo_index,
 
 
 if __name__ == '__main__':
-    ######################################################################
-    amount_to_send = 0.1
-    txid_to_spend = ('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') # TxHash of UTXO
+    amount_to_send = 0.011
+    txid_to_spend = ('59e5ab4ad9e55b55a57138ef323c58208521f0ea6ac7f232e19aabb88565586d') # TxHash of UTXO
     utxo_index = 0 # UTXO index among transaction outputs
+    
     ######################################################################
 
     print(my_address) # Prints your address in base58
     print(my_public_key.hex()) # Print your public key in hex
     print(my_private_key.hex()) # Print your private key in hex
-    txout_scriptPubKey = P2PKH_scriptPubKey(my_address)
+    txout_scriptPubKey = P2PKH_scriptPubKey_out(my_address)
     response = send_from_P2PKH_transaction(amount_to_send, txid_to_spend, utxo_index, txout_scriptPubKey)
     print(response.status_code, response.reason)
     print(response.text) # Report the hash of transaction which is printed in this section result
